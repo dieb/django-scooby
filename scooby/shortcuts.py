fe5601378336f57997cfa55b587e3ba780c92fa2
André Dieb Martins <andre.dieb@gmail.com>
2012-11-25 16:32:15 -0300

    shortcuts: let send receive a single recipient or a list

--- scooby/shortcuts.py
+++ scooby/shortcuts.py
@@ -1,8 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from django.contrib.auth.models import User
+
 from .models import NoticeType, Notice
 
 def send(notice_type_name, recipient, context={}, sender=None):
-    notice_type = NoticeType.objects.get_or_create(name=notice_type_name)
-    notice = Notice.objects.create_notice(notice_type, recipient=recipient, sender=sender)
-    return notice.send(context)
+    notice_type = NoticeType.objects.get_or_create(name=notice_type_name)[0]
+
+    if isinstance(recipient, User):
+        recipient = [recipient]
+
+    notices = []
+
+    for rec in recipient:
+        notice = Notice.objects.create_notice(notice_type, recipient=rec, sender=sender)
+        notice.send(context)
+        notices.append(notice)
+
+    return notices
