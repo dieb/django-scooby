b2d8c5035768dd17383085f833a08f434b90c962
André Dieb Martins <andre.dieb@gmail.com>
2012-11-25 13:06:54 -0300

    Autocreate notice types in order to avoid hell of management.py creation

--- scooby/shortcuts.py
+++ scooby/shortcuts.py
@@ -3,6 +3,6 @@
 from .models import NoticeType, Notice
 
 def send(notice_type_name, recipient, context={}, sender=None):
-    notice_type = NoticeType.objects.get(name=notice_type_name)
+    notice_type = NoticeType.objects.get_or_create(name=notice_type_name)
     notice = Notice.objects.create_notice(notice_type, recipient=recipient, sender=sender)
     return notice.send(context)
